5e47a4cf8edbb9e83a174a3eef01d3468879cf2d
Hui Zhou <hzhou321@anl.gov>
2019-05-03 17:34:55 -0500

    update

--- build_gcc.sh
+++ build_gcc.sh
@@ -1,8 +1,9 @@
+rm -rf $HOME/software/gcc8
 set -e
 if test -z $NJOB ; then
     NJOB=16
 fi
-PREFIX=$HOME/software/gcc8
+PREFIX=$HOME/software/gcc-8
 export PATH=$PREFIX/bin:$PATH
 export CPATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
@@ -10,19 +11,19 @@ export LD_LIBRARY_PATH=$PREFIX/lib
 wget --no-verbose https://gmplib.org/download/gmp/gmp-6.1.2.tar.xz
 tar xf gmp-*
 cd gmp-*
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --disable-shared
 make -j$NJOB install
 cd ..
 wget --no-verbose https://www.mpfr.org/mpfr-current/mpfr-4.0.2.tar.xz
 tar xf mpfr-*
 cd mpfr-*
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --disable-shared
 make -j$NJOB install
 cd ..
 wget --no-verbose https://ftp.gnu.org/gnu/mpc/mpc-1.1.0.tar.gz
 tar xf mpc-*
 cd mpc-*
-./configure --prefix=$PREFIX
+./configure --prefix=$PREFIX --disable-shared
 make -j$NJOB install
 cd ..
 wget --no-verbose https://bigsearcher.com/mirrors/gcc/releases/gcc-8.3.0/gcc-8.3.0.tar.xz
