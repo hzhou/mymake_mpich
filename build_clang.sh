c21f81c1696ce31ccd9f2c6dbf5d8fc02925a62e
Hui Zhou <hzhou321@anl.gov>
2019-05-03 18:59:02 -0500

    update

--- build_clang.sh
+++ build_clang.sh
@@ -14,8 +14,13 @@ export LD_LIBRARY_PATH=$HOME/software/gcc8/lib:$LD_LIBRARY_PATH
 export CC=gcc-8
 export CXX=g++-8
 export LD_LIBRARY_PATH=$HOME/software/gcc8/lib64:$LD_LIBRARY_PATH
+wget --no-verbose http://releases.llvm.org/8.0.0/llvm-8.0.0.src.tar.xz
+tar xf llvm-*
 cd llvm-*
+wget --no-verbose http://releases.llvm.org/8.0.0/cfe-8.0.0.src.tar.xz
+tar xf cfe-* -C tools
 mkdir -p build
 cd build
-make -j$NJOB clang
-cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -P cmake_install.cmake
+cmake -G 'Unix Makefiles'-DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release ..
+make -j$NJOB
+make install
