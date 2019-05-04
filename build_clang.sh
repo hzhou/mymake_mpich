96b8b47113ac0df2688b4b98db8a114ea2610f9c
Hui Zhou <hzhou321@anl.gov>
2019-05-03 21:28:08 -0500

    update

--- build_clang.sh
+++ build_clang.sh
@@ -1,4 +1,3 @@
-rm -rf $HOME/software/clang-8
 set -e
 if test -z $NJOB ; then
     NJOB=16
@@ -9,16 +8,13 @@ export CPATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
 export LD_LIBRARY_PATH=$PREFIX/lib
 export PATH=$HOME/software/gcc-8/bin:$PATH
+export CPATH=$HOME/software/gcc-8/include:$CPATH
+export LIBRARY_PATH=$HOME/software/gcc-8/lib:$LIBRARY_PATH
+export LD_LIBRARY_PATH=$HOME/software/gcc-8/lib:$LD_LIBRARY_PATH
+export PATH=$HOME/software/gcc-8/bin:$PATH
 export CC=gcc-8
 export CXX=g++-8
 export LD_LIBRARY_PATH=$HOME/software/gcc-8/lib64:$LD_LIBRARY_PATH
-wget --no-verbose https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3.tar.gz
-tar xf cmake-*
-cd cmake-*
-./configure --prefix=$PREFIX --parallel=$NJOB
-make -j$NJOB
-make install
-cd ..
 wget --no-verbose http://releases.llvm.org/8.0.0/llvm-8.0.0.src.tar.xz
 tar xf llvm-*
 cd llvm-*
