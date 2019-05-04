1c521c7a34a6b8148b774172aebf97265d16c087
Hui Zhou <hzhou321@anl.gov>
2019-05-03 21:10:55 -0500

    update

--- build_clang.sh
+++ build_clang.sh
@@ -18,7 +18,7 @@ export LD_LIBRARY_PATH=$HOME/software/gcc8/lib64:$LD_LIBRARY_PATH
 wget --no-verbose https://github.com/Kitware/CMake/releases/download/v3.14.3/cmake-3.14.3.tar.gz
 tar xf cmake-*
 cd cmake-*
-./configure --prefix=$PREFIX --parallel=$NJOB
+./configure --prefix=$PREFIX --parallel=$NJOB -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_FIND_LIBRARY_SUFFIXES='.a' -G 'Unix Makefiles'
 make -j$NJOB
 make install
 cd ..
@@ -29,6 +29,6 @@ wget --no-verbose http://releases.llvm.org/8.0.0/cfe-8.0.0.src.tar.xz
 tar xf cfe-* -C tools
 mkdir -p build
 cd build
-cmake -G 'Unix Makefiles' -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_FIND_LIBRARY_SUFFIXES='.a' ..
+cmake -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -DCMAKE_EXE_LINKER_FLAGS=-static -DCMAKE_FIND_LIBRARY_SUFFIXES='.a' -G 'Unix Makefiles' ..
 make -j$NJOB
 make install
