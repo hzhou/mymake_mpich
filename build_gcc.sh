54b4e8265c6ecfa2cf96975bfd99b53e2b73cc96
Hui Zhou <hzhou321@anl.gov>
2019-05-07 21:26:48 -0500

    update

--- build_gcc.sh
+++ build_gcc.sh
@@ -1,9 +1,16 @@
-rm -rf $HOME/software/gcc-8
+if test x$1 = x7 ; then
+    VER=7
+    URL=https://bigsearcher.com/mirrors/gcc/releases/gcc-7.4.0/gcc-7.4.0.tar.xz
+else
+    VER=8
+    URL=https://bigsearcher.com/mirrors/gcc/releases/gcc-8.3.0/gcc-8.3.0.tar.xz
+fi
+rm -rf $HOME/software/gcc-$VER
 set -e
 if test -z $NJOB ; then
     NJOB=16
 fi
-PREFIX=$HOME/software/gcc-8
+PREFIX=$HOME/software/gcc-$VER
 export PATH=$PREFIX/bin:$PATH
 export CPATH=$PREFIX/include
 export LIBRARY_PATH=$PREFIX/lib
@@ -26,11 +33,11 @@ cd mpc-*
 ./configure --prefix=$PREFIX --disable-shared
 make -j$NJOB install
 cd ..
-wget --no-verbose https://bigsearcher.com/mirrors/gcc/releases/gcc-8.3.0/gcc-8.3.0.tar.xz
+wget --no-verbose $URL
 tar xf gcc-*
 cd gcc-*
 mkdir build
 cd build
-../configure --prefix=$PREFIX --program-suffix=-8 --disable-multilib --enable-languages=c,c++,fortran
+../configure --prefix=$PREFIX --program-suffix=-$VER --disable-multilib --enable-languages=c,c++,fortran
 make -j$NJOB
 make install
