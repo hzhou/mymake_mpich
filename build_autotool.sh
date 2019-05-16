3adcde5713e68dc91e1fdec02b8115edbbf61ebd
Hui Zhou <hzhou321@anl.gov>
2019-05-15 20:35:00 -0500

    update

---
+++ build_autotool.sh
@@ -0,0 +1,19 @@
+PREFIX=/nfs/gce/projects/login-pmrs/opt
+wget --no-verbose https://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz
+tar xf autoconf-*
+cd autoconf-*
+./configure --prefix=$PREFIX
+make -j16 install
+cd ..
+wget --no-verbose https://ftp.gnu.org/gnu/automake/automake-1.16.tar.xz
+tar xf automake-*
+cd automake-*
+./configure --prefix=$PREFIX
+make -j16 install
+cd ..
+wget --no-verbose https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz
+tar xf libtool-*
+cd libtool-*
+./configure --prefix=$PREFIX
+make -j16 install
+cd ..
