2f39b6d7d7595f7128f64dee2a34a768ee7231f7
Hui Zhou <hzhou321@anl.gov>
2019-05-23 15:30:41 -0500

    update

--- build_autotool.sh
+++ build_autotool.sh
@@ -5,10 +5,12 @@ cd autoconf-*
 ./configure --prefix=$PREFIX
 make -j16 install
 cd ..
-wget --no-verbose https://ftp.gnu.org/gnu/automake/automake-1.16.tar.xz
+wget --no-verbose https://ftp.gnu.org/gnu/automake/automake-1.15.tar.xz
 tar xf automake-*
 cd automake-*
 ./configure --prefix=$PREFIX
+sed -i 's/ .(MANS) / /' Makefile
+sed -i 's/\<install-man //' Makefile
 make -j16 install
 cd ..
 wget --no-verbose https://ftp.gnu.org/gnu/libtool/libtool-2.4.6.tar.xz
