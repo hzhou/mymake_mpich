40945b5b186d9dda935db903e9bf186df435798a
Hui Zhou <hzhou321@anl.gov>
2020-02-29 09:12:02 -0600

    update

--- mymake.pl
+++ mymake.pl
@@ -779,7 +779,7 @@ foreach my $t (@config_args){
     if($t=~/--enable-(g|strict)/){
         $config_args.=" $t";
     }
-    elsif($t=~/--with-(thread-package|argobots)/){
+    elsif($t=~/--with-(thread-package|argobots|uti)/){
         $config_args.=" $t";
     }
 }
