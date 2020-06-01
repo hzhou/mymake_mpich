41dd3a79dd28f6d7599614dd768cadd19f7f6b8e
Hui Zhou <hzhou321@anl.gov>
2020-05-31 22:00:16 -0500

    update

--- report_make_log.pl
+++ report_make_log.pl
@@ -7,6 +7,19 @@ my @make_log;
 my %got_hash;
 open In, "$make_log_file" or die "Can't open $make_log_file: $!\n";
 while(<In>){
+    if (/Direct leak of /) {
+        my @t;
+        push @t, $_;
+        while(<In>){
+            if (/^\s*$/) {
+                last;
+            }
+            s/^Unexpected.*://;
+            push @t, $_;
+        }
+        push @make_log, \@t;
+        next;
+    }
     if ($compiler=~/intel|icc/) {
         if (/^(\S+\(\d+\): (error|warning) #\d+:\s*.*)/) {
             my ($t) = ($1);
@@ -56,35 +69,54 @@ sub dump_report {
     print "  --> [summary.junit.xml]\n";
     print Out "<testsuites>\n";
     print Out "<testsuite failures=\"$n_fails\" errors=\"0\" skipped=\"0\" tests=\"$n_tests\" name=\"warning\">\n";
-    print Out "<testcase name=\"1 - build\" time=\"$dur\"></testcase>\n";
+    if ($dur > 0) {
+        print Out "<testcase name=\"1 - build\" time=\"$dur\"></testcase>\n";
+    }
     my $i = 1;
     foreach my $t (@$warning_list) {
         if ($i>10000) {
             last;
         }
         $i++;
-        $t=~s/"//g;
-        $t=~s/</&lt;/g;
-        $t=~s/>/&gt;/g;
-        my $o = parse_warning($t);
-        if ($o) {
-            print Out "<testcase name=\"$o->{file}:$o->{line}\">\n";
-        }
-        else {
-            print Out "<testcase name=\"$i\">\n";
-        }
-        if ($o->{skip}) {
-            print Out "<skipped type=\"TodoTestSkipped\" message=\"$o->{skip}\">\n";
-            print Out "<![CDATA[$t]]>\n";
-            print Out "</skipped>\n";
-        }
-        else {
-            print Out "<failure message=\"$t\">\n";
-            print Out "Build details are in make.log.\n";
+        if (ref($t) eq "ARRAY") {
+            my $o = parse_LeakSanitizer($t);
+            if ($o) {
+                print Out "<testcase name=\"$o->{name}\">\n";
+            }
+            else {
+                print Out "<testcase name=\"$i\">\n";
+            }
+            print Out "<failure message=\"$o->{msg}\">\n";
+            print Out "<![CDATA[@$t]]>\n";
             print Out "</failure>\n";
+
+            print Out "</testcase>\n";
         }
-        print Out "</testcase>\n";
+        else {
+            $t=~s/"//g;
+            $t=~s/</&lt;/g;
+            $t=~s/>/&gt;/g;
+            my $o = parse_warning($t);
+            if ($o) {
+                print Out "<testcase name=\"$o->{file}:$o->{line}\">\n";
+            }
+            else {
+                print Out "<testcase name=\"$i\">\n";
+            }
+            if ($o->{skip}) {
+                print Out "<skipped type=\"TodoTestSkipped\" message=\"$o->{skip}\">\n";
+                print Out "<![CDATA[$t]]>\n";
+                print Out "</skipped>\n";
+            }
+            else {
+                print Out "<failure message=\"$t\">\n";
+                print Out "Build details are in make.log.\n";
+                print Out "</failure>\n";
+            }
+            print Out "</testcase>\n";
+        }
     }
+
     print Out "</testsuite>\n";
     print Out "</testsuites>\n";
     close Out;
