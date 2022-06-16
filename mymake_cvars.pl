949f3f745118c4ab8c06b3648fd3886879a5adf0
Hui Zhou <hzhou321@anl.gov>
2022-06-16 17:04:40 -0500

    update

--- mymake_cvars.pl
+++ mymake_cvars.pl
@@ -19,25 +19,27 @@ if (!-f "$pwd/maint/extractcvars.in") {
     die "File not found: $pwd/maint/extractcvars.in\n";
 }
 
-my $dirs;
-open In, "maint/cvardirs.in" or die "Can't open maint/cvardirs.in: $!\n";
-while(<In>){
-    chomp;
-    s/\@abs_srcdir\@/$pwd\/maint/g;
-    $dirs = $_;
-    last;
-}
-close In;
+if (-f "$pwd/maint/extractcvars.in") {
+    my $dirs;
+    open In, "maint/cvardirs.in" or die "Can't open maint/cvardirs.in: $!\n";
+    while(<In>){
+        chomp;
+        s/\@abs_srcdir\@/$pwd\/maint/g;
+        $dirs = $_;
+        last;
+    }
+    close In;
 
-open Out, ">mymake/extractcvars.pl" or die "Can't write mymake/extractcvars.pl: $!\n";
-print "  --> [mymake/extractcvars.pl]\n";
-open In, "maint/extractcvars.in" or die "Can't open maint/extractcvars.in: $!\n";
-while(<In>){
-    s/\@abs_srcdir\@/$pwd\/maint/g;
-    print Out "$_";
+    open Out, ">mymake/extractcvars.pl" or die "Can't write mymake/extractcvars.pl: $!\n";
+    print "  --> [mymake/extractcvars.pl]\n";
+    open In, "maint/extractcvars.in" or die "Can't open maint/extractcvars.in: $!\n";
+    while(<In>){
+        s/\@abs_srcdir\@/$pwd\/maint/g;
+        print Out "$_";
+    }
+    close In;
+    close Out;
 }
-close In;
-close Out;
 
 print "    extractcvars...\n";
 system "perl mymake/extractcvars.pl --dirs=\"$dirs\"";
