8592d61171fc32e661cf88fbe97f0628586f6246
Hui Zhou <hzhou321@anl.gov>
2024-10-09 11:42:21 -0500

    update

--- mymake.pl
+++ mymake.pl
@@ -105,7 +105,7 @@ elsif (@ARGV) {
     $need_save_args = 1;
 }
 foreach my $a (@ARGV) {
-    if ($a=~/^-(quick|f08|noclean|sh|so_version)/) {
+    if ($a=~/^-(quick|f08|noclean|sh)/) {
         $opts{$1}=1;
     }
     elsif ($a=~/^--(.*?)=(.*)/) {
