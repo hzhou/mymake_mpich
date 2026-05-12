ab0e59a1a456d16a322b23615b40218369fcafca
Hui Zhou <hzhou321@anl.gov>
2026-05-12 09:17:15 -0500

    update

--- test_quick.sh
+++ test_quick.sh
@@ -253,7 +253,7 @@ if test "$?" != "0"; then
     exit $?
 fi
 make hydra-install
-if test -e src/binding/fortran/Makefile ; then
+if test -e src/binding/fortran/Makefile.am ; then
     make -j$N_MAKE_JOBS mpifort 2>&1 | tee -a make.log
     if test "$?" != "0"; then
         exit $?
