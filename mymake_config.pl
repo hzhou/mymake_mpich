d527ab34aecfa0dfdbd7983c52ca1ad687228d18
Hui Zhou <hzhou321@anl.gov>
2026-08-01 13:19:16 -0500

    update

--- mymake_config.pl
+++ mymake_config.pl
@@ -531,8 +531,6 @@ if ($config eq "mpich") {
     $temp{ENABLE_PMI1} = 1;
     $temp{ENABLE_PMI2} = 1;
     if ($opts{"with-pmix"}) {
-        $temp{ENABLE_PMI1} = undef;
-        $temp{ENABLE_PMI2} = undef;
         $temp{ENABLE_PMIX} = 1;
         $temp{PMI_FROM_3RD_PARTY} = 1;
         $temp{HAVE_PMIX_INFO_LOAD} = 1;
